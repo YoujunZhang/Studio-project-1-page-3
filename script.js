7bcff2e742ea9351e5e9c241e0d04ff5df42b45f
YoujunZhang <144183143+YoujunZhang@users.noreply.github.com>
2024-02-28 00:03:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function myFunction() {
-    location.replace("https://youjunzhang.github.io/Studio-project-1-page-4//")
+    location.replace("https://youjunzhang.github.io/Studio-project-1-page-3-3/")
   }
